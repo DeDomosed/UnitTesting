bbc4a6a3e4a241ab798e16a21648160729fe39d0
DeDomosed <poperm137@gmail.com>
2024-10-04 14:20:14 +0300

    Added: GCDCalcTest PrimeCalcTest

---
+++ UnitTesting/GCDCalcTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestingLib.Math;
+
+namespace UnitTesting
+{
+    public class GCDCalcTest
+    {
+        private readonly BasicCalc _calculator;
+
+        public GCDCalcTest()
+        {
+            _calculator = new BasicCalc();
+        }
+
+        [Fact]
+        public void GCD_ShouldReturnCorrect()
+        {
+            int result = _calculator.GCD(15, 5);
+            Assert.Equal(5, result);
+      
+        }
+
+        [Fact]
+        public void GCD_ShouldThrowExpection()
+        {
+            var expectionType = typeof(ArgumentOutOfRangeException);
+            Assert.Throws<ArgumentOutOfRangeException>(() =>  _calculator.GCD(-1,5));
+        }
+
+        [Theory]
+        [InlineData(10, 5, 5)]
+        [InlineData(6, 3, 3)]
+        [InlineData(15, 6, 3)]
+        [InlineData(0, 6, 0)]
+        [InlineData(6, 0, 0)]
+
+        public void GCD_Theory(int a, int b, int expectedResult)
+        {
+            int result = _calculator.GCD(a, b);
+            Assert.Equal(expectedResult, result);
+        }
+
+        
+    }
+}
---
+++ UnitTesting/PrimeCalcTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestingLib.Math;
+
+namespace UnitTesting
+{
+    public class PrimeCalcTest
+    {
+        private readonly BasicCalc _calculator;
+
+        public PrimeCalcTest()
+        {
+            _calculator = new BasicCalc();
+        }
+
+        [Fact]
+        public void Prime_ShouldReturnCorrect()
+        {
+            bool result = _calculator.IsPrime(7);
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void Prime_ShouldThrowExpection()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => _calculator.IsPrime(-1));
+        }
+
+        [Theory]
+        [InlineData(1, false)]
+        [InlineData(2, true)]
+        [InlineData(53, true)]
+        [InlineData(0, false)]
+
+        public void Prime_Theory(int a, bool expectedResult)
+        {
+            bool result = _calculator.IsPrime(a);
+            Assert.Equal(expectedResult, result);
+        }
+
+
+    }
+}
